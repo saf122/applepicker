cf20926a039f0c0b30f383f2869da9b955dd650a
saf122 <60560498+saf122@users.noreply.github.com>
2020-12-08 18:45:10 -0500

    Add files via upload

---
+++ AppleTree.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+
+    [Header("Set in Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 20f;
+    public float chanceToChangeDirections = 0.001f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //dropping apples every second   
+      Invoke("DropApple", 2f);
+    }
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //changing direction
+
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } else if (UnityEngine.Random.value < chanceToChangeDirections ) {
+            speed *= -1;
+        }
+    }
+}
---
+++ Basket.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the current screen pos from the mouse
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3d = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+        
+    }
+    void OnCollisionEnter(Collision coll) {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+        }
+        //Parse the text of scoreGT into int
+        int score = int.Parse(scoreGT.text);
+        score += 100;
+        //Convert score back to string
+        scoreGT.text = score.ToString();
+        //track high score
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+    }
+}
